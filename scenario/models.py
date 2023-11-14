a246f0fea56cdce7d01d768119c0f190b448e5c7
OSSU <o.ss.u2056@gmail.com>
2023-11-14 23:40:57 +0900

    Feat: gpt 모델 수정

--- scenario/models.py
+++ scenario/models.py
@@ -41,12 +41,6 @@ class Goal(models.Model):
 
 # GPT 모델
 class Gpt(models.Model):
-    ROLE_CHOICES = (
-        ('system', 'system'),
-        ('assistant', 'assistant'),
-        ('user', 'user'),
-    )
-        
     # gpt_id : 자동 생성 (PK)
     gpt_id = models.BigAutoField(primary_key=True)
     
@@ -54,7 +48,7 @@ class Gpt(models.Model):
     script = models.ForeignKey(Script, related_name='gpts', on_delete=models.CASCADE, db_column="script_id")
     
     # 역할
-    role = models.CharField(choices=ROLE_CHOICES, max_length=10, null=False, blank=False)
+    role = models.CharField(default='', max_length=10, null=False, blank=False)
     # 쿼리
     query = models.TextField(default='', null=True)
     
